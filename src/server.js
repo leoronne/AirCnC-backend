6a9a3d05aab3f7df15487268ab38f749f89ab7a9
Leonardo Ronne <leoronne@gmail.com>
2019-10-07 21:58:46 -0300

    heroku set up

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ app.use(express.json());
 app.use('/files',express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
-
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+});
